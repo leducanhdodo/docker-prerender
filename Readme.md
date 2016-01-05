ed8384e52de9185087e9af644a9fa7aabf92e48c
Joshua Gardner <jgardner@izeni.com>
2016-01-05 16:56:01 -0700

    More plugins activated by environment variables.

--- Readme.md
+++ Readme.md
@@ -4,4 +4,26 @@ Dockerfile to build the prerender container image
 
 ## About
 
-Prerender is a node server from prerender.io that uses phantomjs to create static HTML out of a javascript page. It is perfect for AngularJS SEO, BackboneJS SEO, EmberJS SEO, and any other javascript framework
+Prerender is a node server from prerender.io that uses phantomjs to create
+static HTML out of a javascript page. It is perfect for AngularJS SEO,
+BackboneJS SEO, EmberJS SEO, and any other javascript framework
+
+## Available Environment Variables
+
+- `BASIC_AUTH_USERNAME` and `BASIC_AUTH_PASSWORD` enabled the
+  [basicAuth](https://github.com/prerender/prerender#basicauth) plugin.
+- `ALLOWED_DOMAINS` enables the
+  [whitelist](https://github.com/prerender/prerender#whitelist) plugin.
+- `BLACKLISTED_DOMAINS` enables the
+  [blacklist](https://github.com/prerender/prerender#blacklist) plugin.
+- `S3_BUCKET_NAME` enables the
+  [s3HtmlCache](https://github.com/prerender/prerender#s3htmlcache) plugin.
+    * `AWS_ACCESS_KEY_ID`, required if not using EC2 IAM Roles.
+    * `AWS_SECRET_ACCESS_KEY`, ditto.
+    * `AWS_REGION`, defaults to "us-east-1", set differently if desired.
+- `LOGGER` enableds the [logger](https://github.com/prerender/prerender#logger)
+  plugin.
+- `REDIS_URL` enables the
+  [redis](https://www.npmjs.com/package/prerender-redis-cache) plugin.
+    * `PAGE_TTL` sets the seconds to cache a page. 0 means forever. Default is
+      1 day.
--- server.js
+++ server.js
@@ -1,24 +1,34 @@
-var prerender = require('prerender');
+#!/usr/bin/env node
+var prerender = require('./lib');
 
 var server = prerender({
-	workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
-	iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
-	phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
-	messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
-    });
-
+    workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
+    iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
+    phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
+    messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
+});
 
 server.use(prerender.removeScriptTags());
+server.use(prerender.httpHeaders());
 
-if (process.env.BASIC_AUTH_USERNAME) {
+if ('LOGGER' in process.env) {
+    server.use(prerender.logger());
+}
+if ('BASIC_AUTH_USERNAME' in process.env && 'BASIC_AUTH_PASSWORD' in process.env) {
     server.use(prerender.basicAuth());
 }
-
-server.use(prerender.inMemoryHtmlCache());
-
-//server.use(prerender.blacklist());
-//server.use(prerender.httpHeaders());
-// server.use(prerender.s3HtmlCache());
-// server.use(require('my-plugin'));
+if ('ALLOWED_DOMAINS' in process.env) {
+    server.use(prerender.whitelist());
+}
+if ('BLACKLISTED_DOMAINS' in process.env) {
+    server.use(prerender.blacklist());
+}
+if ('S3_BUCKET_NAME' in process.env) {
+    server.use(prerender.s3HtmlCache());
+} else if ('REDIS_URL' in process.env) {
+    server.use(require('prerender-redis-cache'));
+} else {
+    server.use(prerender.inMemoryHtmlCache());
+}
 
 server.start();
